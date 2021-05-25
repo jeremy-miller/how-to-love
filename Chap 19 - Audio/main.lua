5173b2bb56e1dded9a5def63679db2120fda9e2a
Jeremy Miller <jeremymiller@fastmail.com>
2021-05-25 06:56:06 -0700

    Add chap 19 files

---
+++ Chap 19 - Audio/main.lua
@@ -0,0 +1,13 @@
+function love.load()
+  song = love.audio.newSource("song.ogg", "stream")
+  song:setLooping(true)
+  song:play()
+
+  sfx = love.audio.newSource("sfx.ogg", "static")
+end
+
+function love.keypressed(key)
+  if key == "space" then
+    sfx:play()
+  end
+end
